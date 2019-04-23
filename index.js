5f5a7c20b809553bc7e56b50df5dd4d7dc57b352
cristian3629 <cristian3629@gmail.com>
2019-04-23 20:56:19 -0300

    final 2

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require ('express');
 var app = express();
 
-
+const port = process.env.PORT || 5000
 app.get ('/',(req,res)=> {
   res.send('server.up')
 });
 
 app.use(express.static('public'));
 
-app.listen(9290, function(){
+app.listen(port, function(){
   console.log('Listen port 8080');
 })
